9e9c615ea3ff3a0906b0f3a57d05a91f9f586943
jramos19-dev <jramos19@southtexascollege.edu>
2021-05-01 22:20:30 -0500

    tried fixing public../morty.png

--- src/components/EmptyHome.vue
+++ src/components/EmptyHome.vue
@@ -3,9 +3,9 @@
     class="flex flex-col items-center justify-center min-h-screen space-x-4 text-6xl text-coolGray-300 font-extralight"
   >
     <div class="flex items-center justify-center space-x-4 animate-bounce">
-      <img src="morty.png" alt="Morty.png" />
+      <img src="../../public/morty.png" alt="Morty.png" />
       <h1 class="">Hello There</h1>
-      <img src="morty.png" alt="Morty.png" />
+      <img src="../../public/morty.png" alt="Morty.png" />
     </div>
   </div>
 </template>
